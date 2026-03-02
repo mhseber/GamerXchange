34a8234e1bcba83816555b62383f57acb6e983ae
mahamudul hasan seber <sebermh@gmail.com>
2026-03-02 23:52:27 +0600

    update Code

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,16 @@ function App() {
       <button className="btn btn-success">Success</button>
       <button className="btn btn-warning">Warning</button>
       <button className="btn btn-error">Error</button>
+      <h1>Part 2</h1>
+      <h2 className="text-4xl">GamerXchange</h2>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
     </>
   );
 }
