f376a7a71ab73eb3f199f822e158c581ce6825d1
mahamudul hasan seber <sebermh@gmail.com>
2026-03-04 04:34:56 +0600

    Update Footer section Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/navbar";
 import Hero from "./components/hero";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Footer />
     </>
   );
 }
